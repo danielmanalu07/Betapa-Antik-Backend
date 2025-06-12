ff161870e78cf6027bf2ca3d4a46c53a11e59076
Daniel Manalu <119675317+danielmanalu07@users.noreply.github.com>
2025-06-12 14:51:26 +0700

    Delete dist directory

--- dist/utils/getBaseUrl.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getBaseUrl = void 0;
-const getBaseUrl = (req) => {
-    return `${req.protocol}://${req.get("host")}`;
-};
-exports.getBaseUrl = getBaseUrl;
